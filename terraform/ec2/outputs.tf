74ef2f3a995fb4223780efaa09bf10a5f692f8ae
David Herron <david@davidherron.com>
2020-11-25 12:14:05 -0800

    Add host names to outputs

--- terraform/ec2/outputs.tf
+++ terraform/ec2/outputs.tf
@@ -1,4 +1,5 @@
 
 output "srv-dns"       { value = aws_instance.srv.*.public_dns }
 output "srv-addresses" { value = aws_instance.srv.*.public_ip }
+output "srv-hostnm"    { value = var.configuration[local.env_name].*.host_name }
 output "srv-ssh"       { value = var.configuration[local.env_name].*.ssh_connect }
