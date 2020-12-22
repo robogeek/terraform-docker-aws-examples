434c6cfcc22588ab233aae7245f70beba7ae3a11
David Herron <david@davidherron.com>
2020-12-21 18:29:10 -0800

    Fix syntax

--- wordpress-docker-ecs/db/tmpl/deploy-tmpl.sh
+++ wordpress-docker-ecs/db/tmpl/deploy-tmpl.sh
@@ -6,7 +6,7 @@ docker compose up \
 
 # Only needed if we use the corresponding variables in docker-compose.yml
 #
-#    --environment WORDPRESS_DB_HOST=${ db_host }
-#    --environment WORDPRESS_DB_USER=${ db_user }
-#    --environment WORDPRESS_DB_PASSWORD=${ db_password }
-#    --environment WORDPRESS_DB_NAME=${ db_name }
+#    --environment WORDPRESS_DB_HOST=$ { db_host }
+#    --environment WORDPRESS_DB_USER=$ { db_user }
+#    --environment WORDPRESS_DB_PASSWORD=$ { db_password }
+#    --environment WORDPRESS_DB_NAME=$ { db_name }
