e311e1e4a8ba843c14a0ffbbf07281a5c9fd9cd1
David Herron <david@davidherron.com>
2020-12-24 12:05:59 -0800

    Use AWS Secrets Manager

---
+++ wordpress-docker-ecs/secrets.tf
@@ -0,0 +1,17 @@
+variable "secret-name" {
+    default = "wpdb-access"
+}
+
+resource "aws_secretsmanager_secret" "wpdb-access" {
+    name = var.secret-name
+}
+
+resource "aws_secretsmanager_secret_version" "wpdb-access" {
+    secret_id     = aws_secretsmanager_secret.wpdb-access.id
+    secret_string = jsonencode({
+        db_host: aws_db_instance.wordpress.address,
+        db_user: var.db_username,
+        db_password: var.db_passwd,
+        db_name: var.db_name
+    })
+}
