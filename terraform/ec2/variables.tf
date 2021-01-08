15d706c4bbcc4e3a85a66310be157cd435db9169
David Herron <david@davidherron.com>
2021-01-08 15:21:59 -0800

    Update variables for book

--- terraform/ec2/variables.tf
+++ terraform/ec2/variables.tf
@@ -3,20 +3,27 @@ variable "ami_id" {
     // default = "ami-0d1cd67c26f5fca19"
     // Ubuntu Server 20.04 LTS (HVM), SSD Volume Type - in us-west-2
     default = "ami-09dd2e08d601bff67"
+
+    type        = string
+    description = "The id of the machine image (AMI) to use for the server."
+
+    validation {
+        condition     = length(var.ami_id) > 4 && substr(var.ami_id, 0, 4) == "ami-"
+        error_message = "The image_id value must be a valid AMI id, starting with \"ami-\"."
+    }
 }
 
-variable "instance_type"  { default = "t2.micro" }
-variable "key_pair"       { default = "notes-app-key-pair" }
+variable "key_pair"       { type = string }
 
 variable "instances" {
-    /* type = map(list(object({
+    /* type = list(object({
         name = string,
         instance_type = string,
         associate_public_ip_address = bool,
         host_name = string,
         ssh_connect = string,
         swarm_init = bool
-    }))) */
+    })) */
     default = [
         {
             name = "ec2-srv1",
