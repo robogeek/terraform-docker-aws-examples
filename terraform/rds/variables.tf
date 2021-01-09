ac5e50f604c3816239ec307b9cf835c21c29ebeb
David Herron <david@davidherron.com>
2021-01-08 17:49:06 -0800

    Refactor to use overridable variables for config

--- terraform/rds/variables.tf
+++ terraform/rds/variables.tf
@@ -4,31 +4,12 @@ variable "tododb_username"   { default = "todo" }
 variable "tododb_userpasswd" { default = "passw0rd" }
 variable "tododb_port"       { default = "3306" }
 
-variable "configuration" {
-    type = map(object({
-        allocated_storage = number,
-        storage_type = string,
-        instance_class = string,
-        tododb_id = string,
-        tododb_name = string,
-        tododb_username = string,
-        tododb_userpasswd = string,
-        skip_final_snapshot = bool,
-        multi_az = bool
-    }))
-    default = {
-        "DEV" = {
-            allocated_storage = 20,
-            storage_type = "gp2",
-            instance_class = "db.t2.micro",
-            tododb_id = "tododb",
-            tododb_name = "todo",
-            tododb_username = "todo",
-            tododb_userpasswd = "passw0rd",
-            // tododb_port = "3306",
-            skip_final_snapshot = true,
-            multi_az = true
-        }
-        // ...
-    }
-}
+variable "allocated_storage" { default = 20 }
+variable "storage_type"      { default = "gp2" }
+variable "instance_class"    { default = "db.t2.micro" }
+variable "skip_final_snapshot" { default = true }
+variable "multi_az"          { default = true }
+variable "publicly_accessible" { default = false }
+
+variable "ingress_extra_cidrs" { default = [] }
+variable "egress_extra_cidrs" { default = [] }
