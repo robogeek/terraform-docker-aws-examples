d98c57e3107d4830060e1328d244eeb13966ad38
David Herron <david@davidherron.com>
2021-01-24 09:12:28 -0800

    Initial Revision

---
+++ todo-docker-ecs/tmpl/convert-tmpl.sh
@@ -0,0 +1,6 @@
+docker compose convert \
+    --environment ALB_ARN="${ alb_arn }" \
+    --environment SSL_CERTIFICATE_ARN="${ certificate_arn }" \
+    --environment SEQUELIZE_CONNECT_ARN="${ sequelize_connect_arn }" \
+    --environment WWWDOMAIN="${ www_domain }" \
+    --environment BASEDOMAIN="${ base_domain }"
