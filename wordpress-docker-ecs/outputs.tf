92c7573fc1de12d9a5d2e10478e0f599d18a6bd9
David Herron <david@davidherron.com>
2021-02-01 22:44:12 -0800

    More outputs

--- wordpress-docker-ecs/outputs.tf
+++ wordpress-docker-ecs/outputs.tf
@@ -12,3 +12,9 @@ output "wp_id"       { value = aws_efs_file_system.wordpress.id }
 output "wp_arn"      { value = aws_efs_file_system.wordpress.arn }
 
 output "efs_id"      { value = aws_efs_file_system.wordpress.id }
+output "efs_dns"     { value = aws_efs_file_system.wordpress.dns_name }
+
+output "alb_dbs"     { value = module.alb-dns.alb_dns }
+output "alb_arn"     { value = module.alb-dns.alb_arn }
+output "alb_id"      { value = module.alb-dns.alb_id }
+output "dns_records" { value = module.alb-dns.dns_records.*.alias }
